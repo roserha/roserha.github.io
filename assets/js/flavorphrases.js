8894898e8979d5e6d99cb4c750d18679cbbe3f44
Rose Rodrigues ✨ <30612573+roserha@users.noreply.github.com>
2025-12-01 01:27:46 -0600

    fixing issues?

--- assets/js/flavorphrases.js
+++ assets/js/flavorphrases.js
@@ -17,18 +17,20 @@ let phrases = [
     "There is a ##% chance you're reading this." // this needs to be the last bc im lazy
 ];
 
-if (Math.random() < 1.0 / 3) {
-    // fun phrase time!
+$(document).ready(function() {
+    if (Math.random() < 1.0 / 3) {
+        // fun phrase time!
 
-    let chosenId = Math.floor(phrases.length * Math.random())
+        let chosenId = Math.floor(phrases.length * Math.random())
 
-    $("#flavor").html(`<i>${phrases[chosenId]}</i>`);
+        $("#flavor").html(`<i>${phrases[chosenId]}</i>`);
 
-    if (chosenId == (phrases.length - 1)) {
-        let probby = 100.0 / (3 * (phrases.length - 1))
+        if (chosenId == (phrases.length - 1)) {
+            let probby = 100.0 / (3 * (phrases.length - 1))
 
-        $("#flavor").text(`There is a ${probby.toFixed(2)}% chance you're reading this.`);
+            $("#flavor").text(`There is a ${probby.toFixed(2)}% chance you're reading this.`);
+        }
+
+        $("#flavornum").text(phrases.length - 1);
     }
-
-    $("#flavornum").text(phrases.length - 1);
-}
+});
